f88ca2da0c11af771dcca40b0e6778c73b007b6e
awol#2301 <awollinux@gmail.com>
2021-01-16 22:56:12 -0500

    Mongo FTW

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 import os
 import yaml
 import discord
+import datetime  
 
 from dotenv import load_dotenv
 from pymongo import MongoClient
@@ -38,18 +39,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-        # debug data
-        # print(message.channel.type)
-        # print(type(message.channel.type))
-
         # check to see if message was sent to bot via DM
     if str(message.channel.type) == "private":
 
         getnumber = tn[str(message.author.id)]
         RemoveID = { "addresses": { "$slice": [0, 1] } ,'_id': 0}
-
-            # TicketNumber = 204
-            # TicketName = 'test'
 
             # run a query to check for tickets
 
@@ -66,20 +60,24 @@ async def on_message(message):
                 print(tn.list_collection_names())
                 TicketName = ticket_found['TicketName']
                 print('Ticketname is ' + TicketName)
+                already_has_ticket = True
 
         # If there is no ticket created make a new one 
         amount = len(tn.list_collection_names()) + 1
         if 'TicketName' not in locals():
-            print('couldnt find ticket')
-            print(tn.list_collection_names())
-            print('before ' + str(len(tn.list_collection_names())))
             TicketName = 'ticket-' + str(amount)
             user_info = {"TicketName": TicketName, "status" : "active", }
-            print('78 getnumber.insert_one(user_info)')
             getnumber.insert_one(user_info)
+            already_has_ticket = False
+
+            # debug data
+            print('78 getnumber.insert_one(user_info)')
             print(TicketName)
             print(tn.list_collection_names())
             print(len(tn.list_collection_names()))
+            print('couldnt find ticket')
+            print(tn.list_collection_names())
+            print('before ' + str(len(tn.list_collection_names())))
 
         collection = db[str(TicketName)]
         
@@ -94,27 +92,31 @@ async def on_message(message):
 
         # initilize ticket if user hasn't created one yet
         ticket_list = db.list_collection_names()
+        print(db.list_collection_names())
         print(ticket_list)
-        if str(message.author.id) not in ticket_list:
-        #            TicketCount = len(ticket_list)
-        #            TicketName = 'ticket-' + str(TicketCount)
+        if not already_has_ticket:
             print(TicketName)
             user_info = {
                 "uid" : message.author.id,
                 "author": message.author.name + '#' + message.author.discriminator,
                 "channel": message.channel.id,
-        #                "TicketNumber": TicketNumber,
                 "TicketName" : TicketName,
+                "Count" : 0,
                 "messages" : []
                 }
             
+            print('115 kp.insert_one({"uid" : message.author.id})')
             print('111 collection.insert(user_info)')
             collection.insert(user_info)
-            kp = tn[TicketName]
-            kp.insert_one({"uid" : message.author.id})
-            print('115 kp.insert_one({"uid" : message.author.id})')
+            ticket_list = tn[TicketName]
+            ticket_list.insert_one({"uid" : message.author.id})
 
         collection.update_one(author, update_count)
+
+        # Log message 
+        time = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d-%H:%M:%S-%Z")
+        message_data = {"content": message.content, "author": message.author.name + '#' + message.author.discriminator, "Time" : time }
+        collection.update_one({'uid': message.author.id }, {'$push': {'messages': message_data}})
 
         # get total ticket count
         for total in search:
@@ -137,12 +139,6 @@ async def on_message(message):
             channel = discord.utils.get(guild.text_channels, name=TicketName)
             print(channel)
 
-        # Log message 
-        message_data = {"content": message.content, "author": message.author.name + '#' + message.author.discriminator }
-        collection.update({'uid': message.author.id }, {'$push': {'messages': message_data}})
-        # print(message_data)
-
-
 
         # Debug data
         for result in search:
@@ -159,22 +155,30 @@ async def on_message(message):
 
     # If message is in ticket catagoru\y
     elif message.channel.category_id == 798284727794270229:
-        return
 
         # Figure out whose ticket it was
-    elif False:
                author = {"uid": message.author.id }
                address = { "uid" : {"$exists" : "true" } }
                RemoveID = { "addresses": { "$slice": [0, 1] } ,'_id': 0}
                update_count = {"$inc" : { "Count" : 1}}
-               kp = tn[message.channel.name]
-               query = kp.find(address)
+               ticket_list = tn[message.channel.name]
+               query = ticket_list.find(address)
                for ticket in query:
                    print(ticket['uid'])
                    uid = ticket['uid']
-               collection = db[str(uid)]
+               collection = db[message.channel.name]
+               print(uid)
 
-               search = collection.find(author, RemoveID)
+               # Log message
+               time = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d-%H:%M:%S-%Z")
+               message_data = {"content": message.content, "author": message.author.name + '#' + message.author.discriminator, "Time" : time }
+               collection.update_one({'uid': uid }, {'$push': {'messages': message_data}})
+               update_count = {"$inc" : { "Count" : 1}}
+               owner = {"uid": int(uid) }
+               collection.update_one(owner, update_count)
+
+
+               search = collection.find(owner, RemoveID)
                for total in search:
                    print('#############################################################################################')
                    print(total)
@@ -197,16 +201,5 @@ async def on_message(message):
 
                embedVar = discord.Embed(title=message.author.name + '#' + message.author.discriminator,description=message.content ,inline=False)
                await DM.send(embed=embedVar)
-
-
-               # Log message
-               message_data = { "$set":{ str(Count):{
-                    "content": message.content,
-                    "author": message.author.name + '#' + message.author.discriminator, 
-                    }}}
-               collection.update_one(author, message_data)
-               print(message_data)
-
-               # Debug data
 
 client.run(TOKEN)
