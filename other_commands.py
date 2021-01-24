ad96004f9705b6f3481115b29d96421744629ff0
awol#2304 <awollinux@gmail.com>
2021-01-24 08:57:53 -0500

    Close responds if the user has left and added force close

--- other_commands.py
+++ other_commands.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 import discord
-import mongo 
+import mongo
 log_channel_id = 797996052074201088
 class admin(commands.Cog):
     def __init__(self, bot):
@@ -9,19 +9,56 @@ class admin(commands.Cog):
 
     @commands.has_permissions(administrator=True)
     @commands.command(name='close', help='Archives the complaint putting a full log in admin log')
-    async def clear(self, ctx):
+    async def close(self, ctx):
         TicketName = ctx.channel.name
-        embedVar = discord.Embed(title=TicketName, inline=False)
-        for message in mongo.search.get_messages_by_tickets(TicketName):
-            embedVar.add_field(name=message['author'] + '\n' + message['Time'], value=message['content'] , inline=False)
-
-        admin_log = await self.bot.fetch_channel(log_channel_id)
         owner = await self.bot.fetch_user(mongo.search.get_owner(TicketName))
-        await owner.send(content=f'Hey {owner.name} {TicketName} has been closed here is a transcript. Please feel free to contact us if necessary', embed=embedVar)
+        try:
+            await owner.send(content=f'Hey {owner.name} {TicketName} has been closed here is a transcript. Please feel free to contact us if necessary')
+        except:
+            await ctx.reply('owner has left the server please use force_close')
+            return
+        count = 0
+        embedVar = discord.Embed(title=TicketName, inline=False)
+        admin_log = await self.bot.fetch_channel(log_channel_id)
+        for message in mongo.search.get_messages_by_tickets(TicketName):
+            if count > 25:
+                await admin_log.send(embed=embedVar)
+                await owner.send(embed=embedVar)
+                count = 1
+                embedVar = discord.Embed(title=TicketName + '(cont)', inline=False)
+            if len(message['content']) > 1000:
+                embedVar.add_field(name=message['author'] + '\n' + message['Time'], value=message['content'][0:1000], inline=False)
+                embedVar.add_field(name=message['author'] + ' (cont)', value=message['content'][1000:2005] , inline=False)
+            else:
+                embedVar.add_field(name=message['author'] + '\n' + message['Time'], value=message['content'] , inline=False)
+                count = count + 1
+        await owner.send(embed=embedVar)
+
         mongo.search.archive_channel(TicketName)
         await admin_log.send(embed=embedVar)
         await ctx.channel.delete()
     
-#    @commands.command(name='fs', help='have the bot send the user a friend request')
-#    async def sf(self, ctx):
-#        await ctx.author.send_friend_request()
+
+    @commands.has_permissions(administrator=True)
+    @commands.command(name='force_close', help='Archives the complaint putting a full log in admin log')
+    async def force_close(self, ctx):
+        TicketName = ctx.channel.name
+        owner = await self.bot.fetch_user(mongo.search.get_owner(TicketName))
+        count = 0
+        embedVar = discord.Embed(title=TicketName, inline=False)
+        admin_log = await self.bot.fetch_channel(log_channel_id)
+        for message in mongo.search.get_messages_by_tickets(TicketName):
+            if count > 25:
+                await admin_log.send(embed=embedVar)
+                count = 1
+                embedVar = discord.Embed(title=TicketName + '(cont)', inline=False)
+            if len(message['content']) > 1000:
+                embedVar.add_field(name=message['author'] + '\n' + message['Time'], value=message['content'][0:1000], inline=False)
+                embedVar.add_field(name=message['author'] + ' (cont)', value=message['content'][1000:2005] , inline=False)
+            else:
+                embedVar.add_field(name=message['author'] + '\n' + message['Time'], value=message['content'] , inline=False)
+                count = count + 1
+
+        mongo.search.archive_channel(TicketName)
+        await admin_log.send(embed=embedVar)
+        await ctx.channel.delete()
